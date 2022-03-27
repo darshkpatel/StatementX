6b1583bcdb4e7ecbd88d19aeb7659c60abc417d2
Darsh <darshkpatel@gmail.com>
2022-03-27 14:55:53 +0530

    hotfix: fallback paths

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
     NEXT_PUBLIC_ASSET_PREFIX: '/StatementX/',
   },
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/StatementX',
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '/StatementX/'
 }
